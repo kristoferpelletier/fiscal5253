f9be1bc6f01c6db7b96097848dce979112523562
Kristofer <58676225+kristoferpelletier@users.noreply.github.com>
2023-04-23 20:45:24 -0500

    Update fiscalyearv1.py
    more additions. getting convoluted though

--- fiscal5253/fiscalyearv1.py
+++ fiscal5253/fiscalyearv1.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
 import calendar
 
 class FiscalYear(date):
@@ -9,12 +10,13 @@ class FiscalYear(date):
     firstWeekday = None
 
     @staticmethod
-    def set_example(year: int, examplestart: date, exampleend: date):
+    def set_example(year: int, examplestart: date, exampleend: date, firstfiscalmonth: int):
         FiscalYear.exampleFiscalYear = year
         FiscalYear.exampleStartDate = examplestart
         FiscalYear.exampleEndDate = exampleend
-        FiscalYear.firstMonth = examplestart.month
+        FiscalYear.firstMonth = firstfiscalmonth
         FiscalYear.firstWeekday = examplestart.weekday()
+        
 
     def weekday(self):
         # Since this is a custom class, I want to have Saturday as the beginning of the week.
@@ -34,30 +36,52 @@ class FiscalYear(date):
         self.dt = date(year, month, day)
         self.yr = year
         self.mnth = self.dt.month
+        self.lastyear = year - 1
+        self.sameDateLastYear = self.dt - relativedelta(years=1)
         
-    def leap_year_check(self):
-        return self.yr % 4 == 0 and (self.yr % 100 != 0 or self.yr % 400 == 0)
+    def leap_year_check(self, year):
+        return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
     
-    def weeks_in_the_year(self):
-        weekday = self.weekday()
-        leapyear = self.leap_year_check()
+    def weeks_in_the_prior_year(self):
+        weekday = (date(self.lastyear, self.firstMonth, 1).weekday() + 7 - self.firstWeekday) % 7
+        print(f'theweekday method in weeks in the prior year is {weekday}')
+        leapyear = self.leap_year_check(self.lastyear)
 
         if weekday == 2 and leapyear or weekday == 3 and not leapyear:
             return 53
         else:
             return 52
         
+    def smallest_day_in_first_month_first_week(self):
+
+        firstOfFirstMonth = date(self.year, self.firstMonth, 1)
+        firstMonthFirstWeekday = firstOfFirstMonth.weekday()
+
+        firstday = 1
+
+        if (firstMonthFirstWeekday >= 3 and  firstMonthFirstWeekday <= 5) or self.weeks_in_the_prior_year() == 53:
+            firstday += (5 - firstMonthFirstWeekday)
+        return firstday
+
+    def first_of_fiscal_year(self):
+        pass
+
+
+    def fiscal_year(self):
+        pass
 
 
 
 
 
-fy = FiscalYear(2023, 4, 23)
-fy.set_example(2022, date(2022,1,29), date(2023,2,3))
+fy = FiscalYear(2024, 4, 23)
+fy.set_example(2022, date(2022,1,29), date(2023,2,3), 2)
 
 print(fy.dt)
 print(fy.weekday())
-print(fy.weeks_in_the_year())
+print(fy.weeks_in_the_prior_year())
 print(fy.first_day_of_week())
 print(fy.end_of_previous_week())
 print(fy.start_of_previous_week())
+
+print(fy.smallest_day_in_first_month_first_week())
