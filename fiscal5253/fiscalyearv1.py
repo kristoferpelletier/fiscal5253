5fc5239f29d37e86a097a1dffcf414f996abb623
Kristofer <58676225+kristoferpelletier@users.noreply.github.com>
2023-04-23 19:57:36 -0500

    Update fiscalyearv1.py
    just more additions

--- fiscal5253/fiscalyearv1.py
+++ fiscal5253/fiscalyearv1.py
@@ -6,6 +6,7 @@ class FiscalYear(date):
     exampleStartDate = None
     exampleEndDate = None
     firstMonth = None
+    firstWeekday = None
 
     @staticmethod
     def set_example(year: int, examplestart: date, exampleend: date):
@@ -13,26 +14,33 @@ class FiscalYear(date):
         FiscalYear.exampleStartDate = examplestart
         FiscalYear.exampleEndDate = exampleend
         FiscalYear.firstMonth = examplestart.month
+        FiscalYear.firstWeekday = examplestart.weekday()
 
     def weekday(self):
         # Since this is a custom class, I want to have Saturday as the beginning of the week.
-        return (super().weekday() + 2) % 7
+        # I am also using the example to figure out when the first day of the week should be
+        return (super().weekday() + 7 - self.firstWeekday) % 7
+    
+    def first_day_of_week(self):
+        return self - timedelta(days=self.weekday())
+
+    def end_of_previous_week(self):
+        return self.first_day_of_week() - timedelta(days=1)
+    
+    def start_of_previous_week(self):
+        return self.end_of_previous_week() - timedelta(days=6)
 
     def __init__(self, year, month, day):
         self.dt = date(year, month, day)
         self.yr = year
         self.mnth = self.dt.month
         
-    def LeapYearCheck(self):
+    def leap_year_check(self):
         return self.yr % 4 == 0 and (self.yr % 100 != 0 or self.yr % 400 == 0)
-        
-    def get_fiscal_year(self):
-        thisyear = self.yr
-        lastyear = thisyear - 1
     
-    def weeksInTheYear(self):
+    def weeks_in_the_year(self):
         weekday = self.weekday()
-        leapyear = self.LeapYearCheck()
+        leapyear = self.leap_year_check()
 
         if weekday == 2 and leapyear or weekday == 3 and not leapyear:
             return 53
@@ -44,9 +52,12 @@ class FiscalYear(date):
 
 
 
-fy = FiscalYear(2021, 2, 1)
+fy = FiscalYear(2023, 4, 23)
 fy.set_example(2022, date(2022,1,29), date(2023,2,3))
 
 print(fy.dt)
 print(fy.weekday())
-print(fy.weeksInTheYear())
+print(fy.weeks_in_the_year())
+print(fy.first_day_of_week())
+print(fy.end_of_previous_week())
+print(fy.start_of_previous_week())
